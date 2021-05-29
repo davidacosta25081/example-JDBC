bf1594bf1577fd9f6824e29ee7657c92734f920c
David <davidacosta25081@yahoo.com>
2021-05-28 22:47:03 -0500

    AD: Adding, implementing CRUD operations into the DBConnection Class.

--- src/main/java/com/qa/main/Runner.java
+++ src/main/java/com/qa/main/Runner.java
@@ -1,39 +1,78 @@
 package com.qa.main;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import com.qa.utils.DBConfig;
+import java.util.Scanner;
 
 public class Runner {
 
-  public static void main (String[] args) {
-	  
-    try {
-		Connection con = DriverManager.getConnection
-				("jdbc:mysql://localhost:3306/jdbc_example?db_name&serverTimezone=UTC"
-				,"root","password");
-	
-        PreparedStatement ps = con.prepareStatement("INSERT INTO people (name) values (?)");
-        ps.setString(1,"Emilio");
-        ps.execute();
-    
-        ResultSet rs = ps.executeQuery("SELECT * FROM people");
-        while(rs.next()) {
-          System.out.println(String.format("ID: %d, Name: %s", rs.getInt("id"), rs.getString("name")));	
-        }	
-        con.close(); 
-    
-    
-    } catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}	  
-	  
-	  
+	static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws SQLException {
+
+		DBConnection dbCon = new DBConnection();
+		String action = getAction();
+		int actionInt = Integer.parseInt(action);
+
+		try {
+			do {
+				switch (actionInt) {
+				case 1:
+					System.out.println("Enter de name you wanna add to database");
+					String name = scanner.nextLine();
+					dbCon.create(name);
+					break;
+				case 2:
+					System.out.println("Reading all entries");
+					dbCon.readAll();
+					break;
+				case 3:
+					System.out.println("Enter id of Costumer you'd like lookup");
+					int id = scanner.nextInt();
+					scanner.nextLine();
+					dbCon.readOne(id);
+					break;
+				case 4:
+					System.out.println("Enter id of Costumer you'd like to update");
+					int uId = scanner.nextInt();
+					scanner.nextLine();
+					System.out.println("Enter new name for this Costumer id");
+					String uName = scanner.nextLine();
+					dbCon.update(uId,uName);
+					break;
+                case 5:
+					System.out.println("Enter id of Costumer you'd like to delete");
+					int dId = scanner.nextInt();
+					scanner.nextLine();
+					dbCon.delete(dId);
+					break;
+
+				}
+            action = getAction();
+            actionInt = Integer.parseInt(action);
+			} while (actionInt != 6);
+		System.out.println("\nHave a nice day!");
+		} finally {
+			scanner.close();
+			dbCon.tearDown();
+
+		}
+
+	}
+
+	private static String getAction() {
+
+		String menu = "\n\nWELCOME TO THE INTERNAL COSTUMER DATABASE \n\n" + "1 - Create new Costumer \n"
+				+ "2 - Read all Costumers  \n" + "3 - Read One Costumer   \n" + "4 - Update Costumer   \n"
+				+ "5 - Delete Costumer   \n" + "6 - Exit \n\n"
+
+				+ "Please select a number ...";
+
+		System.out.println(menu + "");
+		return scanner.nextLine().toLowerCase();
+
+	}
+  
+  
 	  
 	  
 	  
@@ -44,8 +83,3 @@ public class Runner {
 
 
 
-
-
-
-
-}
